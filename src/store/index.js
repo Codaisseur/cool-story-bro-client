25b514857f6d03d891d7ed09c0c7c21553a35dee
Domenico Gemoli <domenicogemoli@gmail.com>
2020-11-18 17:56:33 +0100

    Add some cypress tests

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,14 @@ import { createStore, applyMiddleware, compose } from "redux";
 import ReduxThunk from "redux-thunk";
 import reducer from "./rootReducer";
 
-const devTools =
+let devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (window.Cypress) {
+  // when we are running tests in cypress, the devtools enhancer
+  // conflicts with other stuff so we need to get rid of it
+  devTools = x => x;
+}
 
 const enhancer = compose(applyMiddleware(ReduxThunk), devTools);
 
